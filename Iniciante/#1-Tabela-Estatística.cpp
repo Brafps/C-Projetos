11f9c41b98c568eafddc781fa294884eba7ee391
Breno Sampaio <br.sampaio@outlook.com>
2022-05-11 09:51:09 -0300

    Create #1-Tabela-Estatística.cpp

---
+++ Iniciante/#1-Tabela-Estatística.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char c_tipo_tabela;
+   
+    printf("*** Tabela para série Estatística ***\n");
+    
+    printf("Na distribuição de frequência, usa ou não intervalo de classe?");
+    printf("Digite S para sim ou N para não");
+    scanf("%c", &c_tipo_tabela);
+   
+    if c_tipo_tabela = "F"{
+        
+    }
+
+   
+
+    /* printf("Numero 1: %.2f\n", numero1);
+    printf("Numero 2: %.2f\n", numero2);
+    printf("Soma: %.2f\n", soma);
+    printf("Subtracao entre o numero 1 e o numero 2: %.2f\n", subtracao);
+    printf("Multiplicacao: %.2f\n", multiplicacao);
+    printf("Divisao entre o numero 1 e o numero 2: %.2f\n", divisao);*/
+
+    system("Pause");
+    return 0;
+}
